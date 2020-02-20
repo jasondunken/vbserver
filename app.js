404155caf61088143e2a47659ba041da626b9c64
jasondunken <jasondunken@yahoo.com>
2020-02-20 14:47:43 -0500

    refactored to use static angular build

--- app.js
+++ app.js
@@ -1,59 +1,38 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 
-const dotenv = require('dotenv')
-
-const indexRouter = require('./routes/index');
-const apiRouter = require('./routes/api');
-const healthcheckRouter = require('./routes/healthcheck');
-
+const dotenv = require('dotenv');
 const cors = require('cors');
 
+const apiRouter = require('./routes/api');
 const db = require('./vbsdb.js');
 
 if (!process.env.PRODUCTION) {
-    dotenv.config();
+  dotenv.config();
 }
-// if you don't set the desired port before instantiating the app express defaults to listening on port 3000
+
 app = express();
-console.log('Server is listening at PORT', process.env.PORT);
+app.set('port', process.env.PORT);
 
 app.use(cors());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
 // default maximum request body size is 100kb
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static_files')));
 
-app.use('/', indexRouter);
 app.use('/api', apiRouter);
-app.use('/healthcheck', healthcheckRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static_files/index.html'));
 });
 
 db.initDB();
+
+app.listen(process.env.PORT, () => {
+  console.log('server listening on port: ', process.env.PORT);
+});
 
 module.exports = app;
